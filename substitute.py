9220b1204305aadd513d5e94e6b74ed00641de72
kono-dada <121090029@link.cuhk.edu.cn>
2023-06-07 20:54:24 +0800

    修改露莉莴名字，师傅->师父

---
+++ substitute.py
@@ -0,0 +1,26 @@
+import json
+import os
+
+
+translated_dir = 'translated'
+keywords = '师傅'
+substitution = '师父'
+
+for file in os.listdir(translated_dir):
+    with open(os.path.join(translated_dir, file), 'r', encoding='utf8') as f:
+        translated_text = json.load(f)
+    for i, sentence in enumerate(translated_text):
+        key = list(sentence.keys())[0]
+        if keywords in sentence[key]:
+            sentence[key] = sentence[key].replace(keywords, substitution)
+            print(file)
+            print(f'{i}: ' + sentence[key])
+            print()
+    with open(os.path.join(translated_dir, file), 'w', encoding='utf8') as f:
+        s = json.dumps(translated_text, ensure_ascii=False, indent=0)
+        s = s.replace('\n}', '}')
+        s = s.replace('\n]', ']')
+        s = s.replace('{\n', '{')
+        s = s.replace('[\n', '[')
+        f.write(s)
+
