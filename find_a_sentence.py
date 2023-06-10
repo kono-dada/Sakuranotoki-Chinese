28718572934977e4624440e5dd8a90afc4b48273
kono-dada <121090029@link.cuhk.edu.cn>
2023-06-10 16:45:11 +0800

    23/06/10

--- find_a_sentence.py
+++ find_a_sentence.py
@@ -3,7 +3,7 @@ import os
 
 
 translated_dir = 'translated'
-keywords = '宫崎'
+keywords = '宫崎莫忆'
 nkeywords = []
 
 for file in os.listdir(translated_dir):
@@ -11,6 +11,6 @@ for file in os.listdir(translated_dir):
         translated_text = json.load(f)
     for i, sentence in enumerate(translated_text):
         if keywords in list(sentence.values())[0]:
-            # print(file)
+            print(file)
             print(f'{i}: ' + list(sentence.values())[0])
-            # print()
+            print()
