0d039e1068bb6071e52daaf59b82bf2e167e16fa
tsukishima1321 <18001751830@163.com>
2023-07-07 08:56:14 +0800

    第四章优化结束 替换桜->樱

--- substitute.py
+++ substitute.py
@@ -2,12 +2,12 @@ import json
 import os
 
 
-translated_dir = 'translated'
-keywords = '宫崎酥酥'  # 宫崎みすゞ 宫崎美鈴 宫崎美铃 宫崎未寄 宫崎美鶴 宫崎美鎏 宫崎美穗 宫崎美津
+translated_dir = "translated"
+keywords = '桜'  # 宫崎みすゞ 宫崎美鈴 宫崎美铃 宫崎未寄 宫崎美鶴 宫崎美鎏 宫崎美穗 宫崎美津
                         # 宫崎美珠 宫崎实苏 宫崎实铃 宫崎蜜柚 宫崎实齿 宫崎水静 宫崎未鈴 宫崎美洲雀
                         # 宫崎美酴 宫崎美洲 宫崎未译 宫崎莫忆 宫崎樱子 宮崎美寿寿 宫崎三津子 宮崎みすゞ
                         # 宮崎实久 宮崎美寿 宫崎酥酥
-substitution = '宫崎心铃'
+substitution = '樱'
 
 for file in os.listdir(translated_dir):
     with open(os.path.join(translated_dir, file), 'r', encoding='utf8') as f:
