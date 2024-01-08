d134fbdcabd4fb0412d29e81a13760e33a8ac697
UnCLAS-Prommer <gitunclasprommer@gmail.com>
2024-01-09 00:11:10 +0800

    04,05部分

--- substitute.py
+++ substitute.py
@@ -3,11 +3,11 @@ import os
 
 
 translated_dir = "translated"
-keywords = '登窑'  # 宫崎みすゞ 宫崎美鈴 宫崎美铃 宫崎未寄 宫崎美鶴 宫崎美鎏 宫崎美穗 宫崎美津
+keywords = '使用人'  # 宫崎みすゞ 宫崎美鈴 宫崎美铃 宫崎未寄 宫崎美鶴 宫崎美鎏 宫崎美穗 宫崎美津
                         # 宫崎美珠 宫崎实苏 宫崎实铃 宫崎蜜柚 宫崎实齿 宫崎水静 宫崎未鈴 宫崎美洲雀
                         # 宫崎美酴 宫崎美洲 宫崎未译 宫崎莫忆 宫崎樱子 宮崎美寿寿 宫崎三津子 宮崎みすゞ
                         # 宮崎实久 宮崎美寿 宫崎酥酥
-substitution = '阶级窑'
+substitution = '佣人'
 
 for file in os.listdir(translated_dir):
     with open(os.path.join(translated_dir, file), 'r', encoding='utf8') as f:
