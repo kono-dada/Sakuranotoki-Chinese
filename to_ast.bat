0e46ebd090e195fafff3d93192977cbec5164cc2
seahwm <seahwm01@gmail.com>
2024-04-13 10:38:44 +0800

    添加破琵琶生产公社汉化CG为子模块, 并修改to_ast.bat来添加CG

--- to_ast.bat
+++ to_ast.bat
@@ -1,4 +1,9 @@
+git submodule init
+git submodule update
 python to_script.py
 
 cd ./VNTextPatch
 VNTextPatch insertlocal ..\script_jp ..\translated_local ..\script --format=artemisast
+cd ..
+
+python replace_cn_cg_path.py
